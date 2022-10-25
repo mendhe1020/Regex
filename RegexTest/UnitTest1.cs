ce2a41370182f998a67d76cb543d6561bb08d22b
mendhe1020 <anurag.mendhe14@gmail.com>
2022-10-25 10:45:06 +0530

    [Anurag] Add: Ms Test

---
+++ RegexTest/UnitTest1.cs
@@ -0,0 +1,41 @@
+using Regexs;
+using System.Xml.Linq;
+
+namespace RegexTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckFirstNameValidation()
+        {
+            Patterns patterns = new Patterns();
+            string FirstName = "Anurag";
+            Assert.IsTrue(patterns.ValidLastName(FirstName));
+        }
+
+        [TestMethod]
+        public void CheckLastNameValidation()
+        {
+            Patterns patterns = new Patterns();
+            string LastName = "Mendhe";
+            Assert.IsTrue(patterns.ValidLastName(LastName));
+        }
+
+        [TestMethod]
+        public void Emailvalidation()
+        {
+            Patterns patterns = new Patterns();
+            string Email = "anurag.mendhe14@gmail.com";
+            Assert.IsTrue(patterns.ValideEmailID(Email));
+        }
+
+        [TestMethod]
+        public void Passwordvalidation()
+        {
+            Patterns patterns = new Patterns();
+            string Password = "A106322@";
+            Assert.IsTrue(patterns.ValidePassword(Password));
+        }
+    }
+}
